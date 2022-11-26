136d3b33281198a7c1ba469d9ddcb3fe1a36c82e
hanszman <victor.hanszman@hotmail.com>
2022-11-26 10:55:13 -0300

    post battle

--- src/controllers/BattleController.js
+++ src/controllers/BattleController.js
@@ -4,7 +4,8 @@ const RepostaClass = require('./RespostaClass');
 // Classe do Controller
 class BattleController {
     async create (req, resp) {
-        console.log('POST');
+        const body = req.body
+        console.log(body);
     }
 }
 
